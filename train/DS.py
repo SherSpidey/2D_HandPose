b09c59794ecab92e69fe9bbe536ff2b2d554fdcc
SherSpidey <627365980@qq.com>
2020-04-07 14:25:44 +0800

    update

--- train/DS.py
+++ train/DS.py
@@ -1,10 +1,12 @@
 from data_funs import *
+import numpy as np
 
 
 class DS(object):
     def __init__(self, data_dir, batch_size, datasize, mode='training'):
         self.data_dir = data_dir
         self.batch_size = batch_size
+        self.datasize=datasize
         self.all_truns = int(datasize / batch_size)
         self.turn = 0
         self.mode = mode
@@ -18,8 +20,26 @@ class DS(object):
 
     # output adjustable batch_size's data
     def NextBatch(self):
+        """
+
+        :return:
+        """
+        """self.turn = np.random.randint(0, self.datasize-self.batch_size)
         self.images = load_image(self.data_dir, index=self.turn, batch_size=self.batch_size)
         self.turn += 1
         if self.turn == self.all_truns:
             self.turn = 1
-        return self.images, self.annotations[(self.turn - 1)*self.batch_size:self.turn*self.batch_size, :]
+        return self.images, self.annotations[self.turn:self.turn + self.batch_size, :]"""
+        image=[]
+        annotation=[]
+        for _ in range(self.batch_size):
+            self.turn = np.random.randint(0, self.datasize)
+            mid_image=load_image(self.data_dir, index=self.turn, batch_size=1)
+            image.append(mid_image[0])
+            annotation.append(self.annotations[self.turn,:])
+
+        image=np.array(image)
+        annotation=np.array(annotation)
+
+        return image,annotation
+
